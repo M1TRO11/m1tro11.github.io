4a57d222150f3b1a946e1f6f4fbc03a53c9cef9a
M1TRO11 <mitrofanov.g.o@inbox.ru>
2022-03-27 19:32:12 +0300

    add 3 files

---
+++ reg/login.php
@@ -0,0 +1,27 @@
+<?php
+
+$link;
+
+function db_connect() //подключение к базе
+{
+	global $cfg;
+	global $link;
+
+    $link = mysqli_connect("localhost", "user", "pass", "db_name");
+
+    if (!$link) {
+        echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+        echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+    mysqli_query($link, "SET NAMES utf8");
+    mysqli_query($link, "SET CHARACTER SET utf8");
+    mysqli_set_charset($link, "utf8");
+}
+function db_disconnect() //дисконект между нами океаны
+{
+	global $link;
+	mysqli_close($link);
+}
+?>
