3a729fadac95d832a10d5aa872fee569983fbe9b
M1TRO11 <mitrofanov.g.o@inbox.ru>
2022-03-29 00:30:02 +0300

    final reg with warnings

--- reg/reg.php
+++ reg/reg.php
@@ -1,33 +1,44 @@
 <?php
 
+session_start();
+
 include ("db.php");
-if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);} }
-if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
-if (empty($login) or empty($password)) { exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!"); }
 
-$login = stripslashes($login);
-$login = htmlspecialchars($login);
-$password = stripslashes($password);
-$password = htmlspecialchars($password);
-$login = trim($login);
-$password = trim($password);
+$login = $_POST['login'];
+$password = $_POST['password'];
+$password_confirmation = $_POST['password_confirmation'];
 
-db_connect();
+if ($password === $password_confirmation){
+  $login = stripslashes($login);
+  $login = htmlspecialchars($login);
+  $password = stripslashes($password);
+  $password = htmlspecialchars($password);
+  $login = trim($login);
+  $password = trim($password);
 
-$sql_str = "SELECT id FROM `AmongAss` WHERE login=".$login;
-$q = @mysqli_query($link, $sql_str);
-$myrow = mysqli_fetch_array($q);
-if (!empty($myrow['id'])) {
-exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
+  db_connect();
+
+  $sql_str = "SELECT id FROM `AmongAss` WHERE login=".$login;
+  $q = @mysqli_query($link, $sql_str);
+  $myrow = mysqli_fetch_array($q);
+  if (!empty($myrow['id'])) {
+    $_SESSION['err_msg'] = "Извините, введённый вами логин уже зарегистрирован. Введите другой логин.";
+    header('Location: index.html');
+  }
+  $sql_str = "INSERT INTO `AmongAss` (login, password) VALUES ('$login', '$password')";
+  $q = @mysqli_query($link, $sql_str);
+  if ($q != '') {
+    $_SESSION['message'] = "Вы успешно зарегистрированы!";
+    header('Location: ../index.html');
+  } else {
+    $_SESSION['err_msg'] = "Произошла ошибка! Попробуйте ещё раз позже...";
+    header('Location: index.html');
+  }
+
+  db_disconnect();
+}else{
+  $_SESSION['err_msg'] = 'Пароли не совпадают';
+  header('Location: index.html');
 }
-$sql_str = "INSERT INTO `AmongAss` (login, password) VALUES ('$login', '$password')";
-$q = @mysqli_query($link, $sql_str);
-if ($q != '') {
-  echo "Ура";
-} else {
-  echo "не ура";
-}
-
-db_disconnect();
 
 ?>
